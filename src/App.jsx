8a81b31fa676321b8be076466a76f50910e4d933
arsh27a <166740471+arsh27a@users.noreply.github.com>
2024-12-19 14:28:54 +0530

    package installed (#4)
    * documentation added
    
    * temp-conversion-improvement
    
    ---------
    
    Co-authored-by: arsh27a <arshdeepkaue08848@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from "./components/HomePage";
 import React from "react";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
